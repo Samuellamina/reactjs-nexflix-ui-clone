3b85a472e3531db86ef1dd47eb2e5aa6f375dc94
Lamina Samuel <samuellamina2@gmail.com>
2021-10-30 16:07:48 +0100

    updated register.jsx

--- src/pages/register/Register.jsx
+++ src/pages/register/Register.jsx
@@ -24,3 +24,31 @@ const Register = () => {
             alt=""
           />
           <button className="loginButton">Sign In</button>
+        </div>
+      </div>
+      <div className="container">
+        <h1>Unlimited movies, TV shows, and more.</h1>
+        <h2>Watch anywhere. Cancel anytime.</h2>
+        <p>
+          Ready to watch? Enter your email to create or restart your membership.
+        </p>
+        {!email ? (
+          <div className="input">
+            <input type="email" placeholder="email address" ref={emailRef} />
+            <button className="registerButton" onClick={handleStart}>
+              Get Started
+            </button>
+          </div>
+        ) : (
+          <form className="input">
+            <input type="password" placeholder="password" ref={passwordRef} />
+            <button className="registerButton" onClick={handleFinish}>
+              Start
+            </button>
+          </form>
+        )}
+      </div>
+    </div>
+  );
+};
+export default Register;
