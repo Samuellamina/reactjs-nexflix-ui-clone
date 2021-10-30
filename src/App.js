5fa77ed7349848520dba5af4e4789568e27df955
Lamina Samuel <samuellamina2@gmail.com>
2021-10-30 15:18:39 +0100

    added watch vid page

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import "./app.scss";
 import Home from "./pages/Home/Home";
+import Watch from "./pages/watch/Watch";
 
 function App() {
   return (
     <div className="App">
-      <Home />
+      <Watch />
     </div>
   );
 }
